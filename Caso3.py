fa73383bbedefce79a6b72bfe803f2766b474c7d
Fernando Sutter <fernando.sutter1@gmail.com>
2018-10-14 17:43:38 -0300

    Adiciona o algoritmo inicial do Caso 3

---
+++ Caso3.py
@@ -0,0 +1,39 @@
+# Best-fit
+# Setup inicial da memória
+print('Best-fit')
+
+tamanho = 10
+memoria = [None] * tamanho
+
+memoria[2] = 'A'
+memoria[6] = 'B'
+memoria[9] = 'G'
+
+print(memoria)
+
+
+# Worst-fit
+# Setup inicial da memória
+print('Worst-fit')
+
+tamanho = 10
+memoria = [None] * tamanho
+
+memoria[2] = 'A'
+memoria[6] = 'B'
+memoria[9] = 'G'
+
+print(memoria)
+
+# First-fit
+# Setup inicial da memória
+print('First-fit')
+
+tamanho = 10
+memoria = [None] * tamanho
+
+memoria[2] = 'A'
+memoria[6] = 'B'
+memoria[9] = 'G'
+
+print(memoria)
